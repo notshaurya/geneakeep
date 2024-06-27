081b8dbfb993765093d293e0781a78515ff40111
Shaurya <socialshaurya@gmail.com>
2024-06-27 22:45:13 +0530

    drag n drop implementation

--- src/components/note.jsx
+++ src/components/note.jsx
@@ -1,14 +1,77 @@
+"use client";
+
+import saveNoteAction from "@/actions/save-note-action";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/card";
 import { cn } from "@/lib/utils";
+import { useDispatch, useSelector } from "react-redux";
 import { Checkbox } from "./checkbox";
 import { Label } from "./label";
 
 function Note({ data }) {
+    const notes = useSelector((state) => state.saveNoteReducer);
+    const dispatch = useDispatch();
     function handleDragStart(e) {
         e.dataTransfer.setData("note", JSON.stringify(data));
     }
+
+    function preventDefault(e) {
+        e.preventDefault();
+    }
+
+    function handleDrop(e) {
+        e.preventDefault();
+        let dropNoteElement;
+        let draggedData = JSON.parse(e.dataTransfer.getData("note"));
+
+        let element = e.target;
+        while (element) {
+            if (element.id == "note") {
+                dropNoteElement = element;
+                break;
+            }
+            element = element.parentNode;
+        }
+
+        if (!dropNoteElement) return;
+
+        let dropId = Number(dropNoteElement.getAttribute("idnumber"));
+        let dragId = draggedData.id;
+
+        if (dropId == dragId) return;
+
+        let newNotes = [...notes];
+
+        let dropIndex;
+        let dragIndex;
+
+        for (let index = 0; index < newNotes.length; index++) {
+            if (dropIndex != undefined && dragIndex != undefined) break;
+            const e = newNotes[index];
+            if (e.id == dragId) {
+                dragIndex = index;
+            }
+            if (e.id == dropId) {
+                dropIndex = index;
+            }
+        }
+
+        newNotes.splice(dragIndex, 1);
+        newNotes.splice(dropIndex, 0, draggedData);
+        dispatch(saveNoteAction(newNotes));
+        localStorage.setItem("data", JSON.stringify(newNotes));
+    }
+
     return (
-        <div id="note" idnumber={data.id} draggable onDragStart={handleDragStart} className="p-4 sm:p-6 active:cursor-move hover:cursor-default  ">
+        <div
+            id="note"
+            idnumber={data.id}
+            draggable
+            onDragStart={handleDragStart}
+            onDrop={handleDrop}
+            onDragOver={preventDefault}
+            onDragEnd={preventDefault}
+            className="p-4 sm:p-6 active:cursor-move hover:cursor-default"
+        >
             <Card className={cn("h-fit overflow-hidden border-[px] border-slate-400 text-clip hover:cursor-pointer ", data.bgColor)}>
                 <CardHeader>
                     <CardTitle className="text-sm font-bold">{data.title}</CardTitle>
--- src/components/notes-area.jsx
+++ src/components/notes-area.jsx
@@ -12,66 +12,13 @@ function NotesArea() {
     const data = useSelector((state) => state.saveNoteReducer);
     const search = useSelector((state) => state.searchReducer);
 
-    function preventDefault(e) {
-        e.preventDefault();
-    }
-
-    function handleDrop(e) {
-        e.preventDefault();
-        let dropNoteElement;
-        let draggedData = JSON.parse(e.dataTransfer.getData("note"));
-
-        let element = e.target;
-        while (element) {
-            if (element.id == "note") {
-                dropNoteElement = element;
-                break;
-            }
-            element = element.parentNode;
-        }
-
-        if (!dropNoteElement) return;
-
-        let dropId = Number(dropNoteElement.getAttribute("idnumber"));
-        let dragId = draggedData.id;
-
-        if (dropId == dragId) return;
-
-        let newData = [...data];
-
-        let dropIndex;
-        let dragIndex;
-
-        for (let index = 0; index < newData.length; index++) {
-            const e = newData[index];
-            if (e.id == dragId) {
-                dragIndex = index;
-            }
-            if (e.id == dropId) {
-                dropIndex = index;
-            }
-        }
-
-        newData.splice(dragIndex, 1);
-        dropIndex = dragIndex < dropIndex ? dropIndex - 1 : dropIndex;
-        newData.splice(dropIndex, 0, draggedData);
-        dispatch(saveNoteAction(newData));
-        localStorage.setItem("data", JSON.stringify(newData));
-    }
-
     useEffect(() => {
         let notes = localStorage.getItem("data");
         dispatch(saveNoteAction(notes ? JSON.parse(notes) : []));
     }, []);
 
     return (
-        <div
-            id="notes-area"
-            className="flex flex-wrap justify-center items-start w-full"
-            onDrop={handleDrop}
-            onDragOver={preventDefault}
-            onDragEnd={preventDefault}
-        >
+        <div id="notes-area" className="flex flex-wrap justify-center items-start w-full">
             {data?.length ? (
                 data
                     .filter((item) => search == "" || (item.title?.toLowerCase() ?? "").includes(search.toLowerCase()))
