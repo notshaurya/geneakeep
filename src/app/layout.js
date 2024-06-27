3ea5ad96ef99cb881a3cb874b08b806d548708b4
Shaurya <socialshaurya@gmail.com>
2024-06-27 20:28:16 +0530

    layout change

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,25 @@
 import StoreProvider from "@/store/storeProvider";
 import "./globals.css";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
 
 export const metadata = {
     title: "Genea Keep",
     description: "A notes app",
 };
 
+const courier = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
+
 export default function RootLayout({ children }) {
     return (
         <StoreProvider>
             <html lang="en" className="flex justify-center">
-                <body className="h-full w-full flex flex-col justify-center items-center max-w-screen-2xl gap-10 p-4 sm:p-10 bg-slate-200">
+                <body
+                    className={cn(
+                        "h-full w-full flex flex-col justify-center items-center max-w-screen-2xl gap-10 p-4 sm:p-10 bg-slate-200",
+                        courier.className
+                    )}
+                >
                     {children}
                 </body>
             </html>
--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import Image from "next/image";
 export default function Home() {
     return (
         <>
-            <div className="w-full shadow-lg bg-slate-100 flex justify-between items-center h-10 py-8 px-4 rounded-lg gap-4">
+            <div className="w-full shadow-lg bg-slate-100 flex justify-between items-center h-fit py-3 px-4 rounded-lg gap-4">
                 <div className="flex gap-2 justify-center items-center">
                     <ScrollText size={35} className="" stroke="orange" />
                     <h2 className=" text-lg sm:text-2xl font-bold text-indigo-600">Genea Keep</h2>
@@ -21,8 +21,8 @@ export default function Home() {
                     </div>
                 </a>
             </div>
-            <h1 className="text-4xl font-bold text-indigo-600">
-                Track your notes <span className="   font-extrabold text-indigo-600">better !</span>
+            <h1 className="text-4xl font-bold text-indigo-600 ">
+                Track your notes <span className="   font-extrabold text-indigo-600">better!</span>
             </h1>
             <AddNote />
             <NotesArea />
